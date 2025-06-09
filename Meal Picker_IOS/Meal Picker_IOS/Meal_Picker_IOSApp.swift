36287e9cef93698300f2200ae4ea229544ca4260
aaloraini <hkem.moh.ar@gmail.com>
2025-06-09 11:25:41 +0800

    IOS App

---
+++ Meal Picker_IOS/Meal Picker_IOS/Meal_Picker_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meal_Picker_IOSApp.swift
+//  Meal Picker_IOS
+//
+//  Created by Abdulhakim Aloraini on 08/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct Meal_Picker_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
